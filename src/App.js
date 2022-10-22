7695fd105ea483ee9820e99cb7307cdeba434702
Giles <gsandoval2277@gmail.com>
2022-10-22 13:26:50 -0600

    added App.js file

---
+++ src/App.js
@@ -0,0 +1,36 @@
+const Pet = (props) => {
+    return (
+        React.createElement("div", {}, [
+            React.createElement("h1", {}, props.name),
+            React.createElement("h2", {}, props.animal),
+            React.createElement("h2", {}, props.breed),
+
+        ])
+    )
+}
+
+
+// analogy: this creates a new kind of car. ex. creating honda civic as new line of cars
+const App = () => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Paco",
+        animal: "Dog",
+        breed: "Chihuah Mix"
+      }),
+      React.createElement(Pet, {
+        name: "Pablo",
+        animal: "Dog",
+        breed: "Chihuah Mix"
+      }),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Pug"
+      }),
+    ]);
+  };
+
+// analogy: this creates one individual car and is ready to have multiple options on that car.
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
