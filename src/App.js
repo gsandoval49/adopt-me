56cc9eea5bceee67a9a52ee98cb69ef97333b941
Giles <gsandoval2277@gmail.com>
2022-11-12 21:09:24 -0700

    added Pet.js file/component and imported line in App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-const Pet = (props) => {
-return React.createElement("div", {}, [
-React.createElement("h1", {}, props.name),
-React.createElement("h2", {}, props.animal),
-React.createElement("h2", {}, props.breed),
-]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
+
 
 // analogy: this creates a new kind of car. ex. creating honda civic as new line of cars
 const App = () => {
-return React.createElement("div", {}, [
-React.createElement("h1", {}, "Adopt Me!"),
-React.createElement(Pet, {
-  name: "Paco",
-  animal: "Dog",
-  breed: "Chihuaha Mix",
-}),
-React.createElement(Pet, {
-  name: "Pablo",
-  animal: "Dog",
-  breed: "Chihuaha Mix",
-}),
-React.createElement(Pet, {
-  name: "Luna",
-  animal: "Dog",
-  breed: "Pug",
-}),
-]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Paco",
+      animal: "Dog",
+      breed: "Chihuaha Mix",
+    }),
+    React.createElement(Pet, {
+      name: "Pablo",
+      animal: "Dog",
+      breed: "Chihuaha Mix",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Pug",
+    }),
+  ]);
 };
 
 // analogy: this creates one individual car and is ready to have multiple options on that car.
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
