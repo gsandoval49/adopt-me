041de5915c5d0911debae31819774fa86ddcfe37
Giles <gsandoval2277@gmail.com>
2022-11-21 21:11:21 -0700

    deleted uneccesary code now that JSX is used

--- src/Pet.js
+++ src/Pet.js
@@ -5,17 +5,7 @@ const Pet = props => {
             <h2>{props.animal}</h2>
             <h2>{props.breed}</h2>
         </div>
-    )
-
-}
-
-
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, props.name),
-//       React.createElement("h2", {}, props.animal),
-//       React.createElement("h2", {}, props.breed),
-//     ]);
-//   };
+    );
+};
 
 export default Pet;
